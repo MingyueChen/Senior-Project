0cb6018970f8ba76169c70b89e1111412c658dbc
MingyueChen <mingyuehappy.com>
2018-10-18 14:36:52 -0400

    Finally contactUs, edit, add, delete functionalties all work

--- backend/app.js
+++ backend/app.js
@@ -42,13 +42,13 @@ mongoose.connect("mongodb+srv://Mingyue:YHscIiTW176wx73Y@kattell-and-company-wyf
   console.log('Connection failed.');
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extented:true}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH,PUT, DELETE, OPTIONS');
   next();
 });
 

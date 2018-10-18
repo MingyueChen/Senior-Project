9a00006b093babbf6dbdc0943e95f1301cf1abfa
MingyueChen <mingyuehappy.com>
2018-10-18 11:38:53 -0400

    delete router and revert homeCreateTS

--- src/app/home/home-create/home-create.component.ts
+++ src/app/home/home-create/home-create.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {  HomeService } from '../home.service';
-
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { EmployeeInfo } from '../home.model';
 
 
 @Component({
@@ -11,16 +12,42 @@ import {  HomeService } from '../home.service';
 })
 export class HomeCreateComponent implements OnInit {
 
-  constructor(public homeService: HomeService) { }
+  private mode = 'create';
+  private employeeID: string;
+  employeeInfo: EmployeeInfo;
+  constructor(public homeService: HomeService, public route: ActivatedRoute) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    // try to find out whether we have an employeeID or not
+    // we can extract this by accessing this route,
+    // so out inject activated route and we have maraMap object or property
+    // paraMap is actually an observable to which we can subscribe
+    // the call back function will be called whenever the parameter changes
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      // we check if it has 'employeeID' because we name it employeeID in app-routing.module.ts
+      if (paramMap.has('employeeID')) {
+        this.mode = 'edit';
+        this.employeeID = paramMap.get('employeeID');
+        this.homeService.getEmployeeInfo(this.employeeID).subscribe(infoData => {
+          this.employeeInfo = {employeeID: infoData._id, employeeEmail: infoData.employeeEmail, employeeName: infoData.employeeName};
+        });
+      } else {
+        this.mode = 'create';
+        this.employeeID = null;
+      }
+    });
+  }
 
-  onAddEmployeeInfo (form: NgForm) {
+  onSaveEmployeeInfo (form: NgForm) {
     if (form.invalid) {
       return;
     }
 
-    this.homeService.addInfo(form.value.employeeName, form.value.employeeEmail);
+    if (this.mode === 'create') {
+      this.homeService.addInfo(form.value.employeeName, form.value.employeeEmail);
+    } else {
+      this.homeService.updateEmployeeInfo(this.employeeID, form.value.employeeName, form.value.employeeEmail);
+    }
     form.resetForm();
   }
 

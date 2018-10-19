b8f3c312e1c8052932f1fd118ab6e4cdc00283ac
MingyueChen <mingyuehappy.com>
2018-10-18 23:53:52 -0400

    reorganize backends and add signup and login pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,18 @@ import { HomeCreateComponent } from './home/home-create/home-create.component';
 import {  HomeListComponent } from './home/home-list/home-list.component';
 import { OfficeLocationComponent } from './office-location/office-location.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeListComponent },
-  {path: 'addEmployeeInfo', component: HomeCreateComponent},
+  { path: 'addEmployeeInfo', component: HomeCreateComponent},
   { path: 'officeLocation', component: OfficeLocationComponent },
   { path: 'contactUs', component: ContactUsComponent },
-  { path: 'edit/:employeeID', component: HomeCreateComponent }
+  { path: 'edit/:employeeID', component: HomeCreateComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({

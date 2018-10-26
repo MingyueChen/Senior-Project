065ef73c99edd9e51b612e0eef0126b0c495ef71
MingyueChen <mingyuehappy.com>
2018-10-26 17:43:26 -0400

    remind administrators when signup or login failed

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-
+export class LoginComponent implements OnInit, OnDestroy {
+  private authStatusSub: Subscription;
   constructor(public authService: AuthService) { }
 
   ngOnInit() {
-  }
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(authStatus => {
 
+    });
+  }
   onLogin(form: NgForm) {
     if (form.invalid) {
       return;
     }
     this.authService.login(form.value.email, form.value.password);
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 
 } // end of LoginComponent class
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,24 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
-
+export class SignupComponent implements OnInit, OnDestroy {
+  private authStatusSub: Subscription;
+  public authStatus;
+  public isKaEmail = false;
   constructor(public authService: AuthService) { }
 
   ngOnInit() {
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(authStatus => {
+
+    });
   }
 
   onSignup(form: NgForm) {
     if (form.invalid) {
       return;
     }
+    if (! (form.value.email.includes('@kattell.com'))) {
+      this.isKaEmail = false;
+      console.log(form.value.email);
+      return;
+    }
+    this.isKaEmail = true;
     this.authService.createAdmin(form.value.email, form.value.password);
   }
+
+    ngOnDestroy() {
+      this.authStatusSub.unsubscribe();
+    }
 
 }
